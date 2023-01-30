98d4d9e8babf8d392673f21200045b274ce90770
Ian Hess <ian.h.hess@gmail.com>
2023-01-30 14:12:13 -0500

    add merge sort

--- app.js
+++ app.js
@@ -11,9 +11,48 @@
 // }
 // console.log(fibs(4));
 
-function fibsRec(n) {
-  if (n < 2) return 1;
+// function fibsRec(n) {
+//   if (n < 2) return 1;
 
-  return fibsRec(n - 1) + fibsRec(n - 2);
+//   return fibsRec(n - 1) + fibsRec(n - 2);
+// }
+// console.log(fibsRec(5));
+
+// function mult(n) {
+//   let sum = 0;
+//   if (mult(n - 1) % 3 === 0 || mult(n - 1) % 5 === 0) {
+//     sum = sum + mult(n - 1);
+//   }
+//   return sum;
+// }
+
+// console.log(mult(10));
+
+function mergeSortTopDown(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const middle = Math.floor(arr.length / 2);
+  const left = arr.slice(0, middle);
+  const right = arr.slice(middle);
+
+  return mergeSortTopDown(mergeSortTopDown(left), mergeSortTopDown(right));
 }
-console.log(fibsRec(5));
+
+function mergeTopDown(left, right) {
+  const arr = [];
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      arr.push(left.shift());
+    } else {
+      arr.push(right.shift());
+    }
+  }
+  return arr.concat(left.slice()).concat(right.slice());
+}
+
+testArr = [1, 8, 2, 0, 9, 10, 188, 23, 3289];
+
+console.log(mergeSortTopDown(testArr));
