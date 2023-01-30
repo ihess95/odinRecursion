66154f8fd5735f00dac25ae7641c4c501dc50fa3
Ian Hess <ian.h.hess@gmail.com>
2023-01-30 13:08:46 -0500

    add functions

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("test");
+// function fibs(n) {
+//   let x = 1;
+//   let t = 0;
+//   let p = 0;
+//   for (let i = 1; i <= n; i++) {
+//     p = x + t;
+//     x = t;
+//     t = p;
+//   }
+//   return p;
+// }
+// console.log(fibs(4));
+
+function fibsRec(n) {
+  if (n < 2) return 1;
+
+  return fibsRec(n - 1) + fibsRec(n - 2);
+}
+console.log(fibsRec(5));
