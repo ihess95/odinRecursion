83e9209f673995b188b391c377cb409b09e11d05
Ian Hess <ian.h.hess@gmail.com>
2023-02-02 15:58:17 -0500

    try making append

--- LinkedList.js
+++ LinkedList.js
@@ -1,12 +1,19 @@
 class LinkedList {
   constructor() {
     this.head = null;
+    this.end = null;
     this.length = 0;
   }
 
   insertAtHead(data) {
     const newNode = new LinkedListNode(data, this.head);
     this.head = newNode;
+    this.length++;
+  }
+
+  append(data) {
+    const newNode = new LinkedListNode(data, this.end);
+    this.end = newNode;
     this.length++;
   }
 }
