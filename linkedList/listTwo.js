86e7036a1edd5eec506003513e562c76f055da0d
Ian Hess <ianhess@Ians-MacBook-Pro.local>
2023-02-04 13:43:47 -0500

    whoops add files

---
+++ linkedList/listTwo.js
@@ -0,0 +1,81 @@
+const LinkedList = () => {
+  let head = null;
+  let length = 0;
+
+  const append = (value) => {
+    const newNode = Node(value);
+    length++;
+    if (head === null) {
+      return (head = newNode);
+    }
+    let pointer = head;
+    while (pointer.nextNode !== null) {
+      pointer = pointer.nextNode;
+    }
+    pointer.nextNode = newNode;
+  };
+
+  const prepend = (value) => {
+    const newNode = Node(value);
+    length++;
+    if (head === null) {
+      return (head = newNode);
+    }
+    newNode.nextNode = head;
+    head = newNode;
+  };
+
+  const size = () => {
+    if (!head) return null;
+    return length;
+  };
+
+  const getHead = () => {
+    if (!head) return null;
+    return head.value;
+  };
+
+  const tail = () => {
+    if (!head) return null;
+    let pointer = head;
+    while (pointer.nextNode !== null) {
+      pointer = pointer.nextNode;
+    }
+    return pointer.value;
+  };
+
+  const at = (index) => {
+    if (!head) return null;
+    let pointer = head;
+    for (let i = 0; i < index; i++) {
+      pointer = pointer.nextNode;
+    }
+    return pointer;
+  };
+
+  const pop = () => {
+    if (!head) return null;
+    if (head.nextNode === null) {
+      head = null;
+    }
+    at(size() - 2).nextNode = null;
+    length--;
+  };
+
+  const cointains = (value) => {
+    if (!head) return null;
+    let pointer = head;
+    while (pointer.nextNode !== null) {
+      if (pointer.value === value) return true;
+      pointer = pointer.nextNode;
+    }
+    return pointer.value === value ? true : false;
+  };
+
+  const find = (value) => {
+    if (!head) return null;
+    let index = 0;
+    let pointer = head;
+    while (pointer.nextNode !== node)
+  };
+};
