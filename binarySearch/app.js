a3680dc6ca367c8fc24f8b783e59b332f235ffb6
Ian Hess <ian.h.hess@gmail.com>
2023-02-06 15:28:49 -0500

    make changes to insert

--- binarySearch/app.js
+++ binarySearch/app.js
@@ -1,47 +1,12 @@
-// class Node {
-//   constructor(d) {
-//     this.data = d;
-//     this.left = null;
-//     this.right = null;
-//   }
-// }
-
-// let root = null;
-
-// function sortedArrayToBST(arr, start, end) {
-//   if (start > end) return null;
-
-//   let mid = parseInt((start + end) / 2);
-//   let node = new Node(arr[mid]);
-
-//   node.left = sortedArrayToBST(arr, start, mid - 1);
-//   node.right = sortedArrayToBST(arr, mid + 1, end);
-// }
-
-// function preOrder(node) {
-//   if (node === null) return;
-//   document.write(node.data + " ");
-//   preOrder(node.left);
-//   preOrder(node.right);
-// }
-
-// let arr = [1, 2, 3, 4, 5, 6, 7];
-// let n = arr.length;
-// root = sortedArrayToBST(arr, 0, n - 1);
-// document.write("Preorder traversal of constructed BST<br>");
-// preOrder(root);
-
-const prettyPrint = (node, isLeft = true) => {
+const prettyPrint = (node, prefix = "", isLeft = true) => {
   if (node.right !== null) {
-    prettyPrint(node.right, `${prefix}${isLeft ? "| " : "   "}`, false);
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
   }
   console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
   if (node.left !== null) {
     prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
   }
 };
-
-// prefix = "";
 
 class Node {
   constructor(d) {
@@ -51,6 +16,47 @@ class Node {
   }
 }
 
-class Tree {}
+class Tree {
+  constructor(input) {
+    this.root = this.buildTree(input, 0, input.length - 1);
+    prettyPrint(this.root);
+  }
 
-function buildTree(arr) {}
+  buildTree(arr, start, end) {
+    if (start > end) return null;
+
+    let mid = parseInt((start + end) / 2);
+    let root = new Node(arr[mid]);
+
+    root.left = this.buildTree(arr, start, mid - 1);
+    root.right = this.buildTree(arr, mid + 1, end);
+    return root;
+  }
+
+  insert(value, root = this.root) {
+    if (root === null) {
+      return (root = new node(value));
+    }
+    if (root.data < value) {
+      root.right = this.insert(value, root.right);
+    } else {
+      root.left = this.insert(value, root.left);
+    }
+    prettyPrint(this.root);
+    return root;
+  }
+
+  delete(value, root = this.root) {
+    if (root === null) return null;
+
+    if (root.data > value) {
+      root.left = this.delete(value, root.left);
+    } else if (root.data < value) {
+      root.right = this.delete(value, root.right);
+    }
+  }
+}
+
+let test = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+balancedBST = new Tree(test, 1, 9);
+console.log(balancedBST);
